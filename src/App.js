34db90139630383c637a59e1159e5bf9e979ff5c
Wandu12 <info10229@gmail.com>
2020-10-10 23:58:06 +0900

    edit app component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() { 
+    /*App()함수 정의 > 해당 함수가 <div><img/><h1>The Movie App by Wandu</h1></div>를 반환. 
+    => App 컴포넌트. App()함수가 반환한 HTML이 리액트 앱 화면에 그려지는render 것*/
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            The Movie App by Wandu
-        </p>
-      </header>
+            <h1>The Movie App by Wandu</h1>
     </div>
   );
 }
