76d4d213c95cf2249d8937d8b7ade9228a8b5aa9
Wandu12 <info10229@gmail.com>
2020-10-11 23:38:15 +0900

    add Potato component(~65p 컴포넌트 동작방식 학습)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Potato from "./Potato";
 
 function App() { 
-    /*App()함수 정의 > 해당 함수가 <div><img/><h1>The Movie App by Wandu</h1></div>를 반환. 
-    => App 컴포넌트. App()함수가 반환한 HTML이 리액트 앱 화면에 그려지는render 것*/
+    // App()함수 정의 > 해당 함수가 <div><img/><h1>The Movie App by Wandu</h1></div>를 반환.
+    // => App 컴포넌트. App()함수가 반환한 HTML이 리액트 앱 화면에 그려지는render 것
+    
+    // 리액트가 아래 <Potato />를 해석해서 <h3>Potato</h3>로 만들어줌 
   return (
     <div>
         <img src={logo} className="App-logo" alt="logo" />
             <h1>The Movie App by Wandu</h1>
+            <Potato />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; //
 import App from './App'; //App 컴포넌트를 import하여 사용한 것
-import * as serviceWorker from './serviceWorker'; //
+import * as serviceWorker from './serviceWorker';
+import Potato from "./Potato"; //
 
 ReactDOM.render(<App />, document.getElementById('root'));
+    //리액트는 <App /> 과 같은 표기를 컴포넌트로 인식하고, 해당 컴포넌트가 반환하는 값을 화면에 그려줌
+    //리액트는 컴포넌트와 함께 동작하며, 리액트 앱은 모두 컴포넌트로 구성됨
+    //리액트는 최종적으로 컴포넌트를 단 한 개만 그린다
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
